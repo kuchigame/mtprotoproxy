f8b1978a7ca4baf8103d7c8793cad04f945f194a
kuchigame <66659933+kuchigame@users.noreply.github.com>
2021-10-02 16:31:52 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,7 @@ PORT = 443
 
 # name -> secret (32 hex chars)
 USERS = {
-    "tg":  "00000000000000000000000000000001",
-    # "tg2": "0123456789abcdef0123456789abcdef",
+    "tg":  "92D469B50A8499D6E10CED6031821DD3",
 }
 
 MODES = {
